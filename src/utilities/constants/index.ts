a9788fb7aa9e390f16c4ff7920bc9b45604846f7
rhitabratakarar <kararrhitabrata@gmail.com>
2024-01-03 00:25:27 +0530

    added constants

---
+++ src/utilities/constants/index.ts
@@ -0,0 +1,27 @@
+export const HEADERS_FOR_GENERIC_PAGE = {
+  accept: "*/*",
+  host: "www.instagram.com",
+  referer: "https://www.instagram.com/",
+  DNT: "1",
+  "Sec-Fetch-Dest": "document",
+  "Sec-Fetch-Mode": "navigate",
+  "Sec-Fetch-Site": "same-origin",
+  "User-Agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0",
+};
+
+export const HEADERS_FOR_GRAPHQL = {
+  "Accept": "*/*",
+  "Accept-Language": "en-US,en;q=0.5",
+  "Content-Type": "application/x-www-form-urlencoded",
+  "X-FB-Friendly-Name": "PolarisPostActionLoadPostQueryQuery",
+  "X-CSRFToken": "RVDUooU5MYsBbS1CNN3CzVAuEP8oHB52",
+  "X-IG-App-ID": "1217981644879628",
+  "X-FB-LSD": "AVqbxe3J_YA",
+  "X-ASBD-ID": "129477",
+  "Sec-Fetch-Dest": "empty",
+  "Sec-Fetch-Mode": "cors",
+  "Sec-Fetch-Site": "same-origin",
+  "User-Agent":
+    "Mozilla/5.0 (Linux; Android 11; SAMSUNG SM-G973U) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/14.2 Chrome/87.0.4280.141 Mobile Safari/537.36",
+};
