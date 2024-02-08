65132f8907b3c9614b2f6b82a32dd643dcda92dd
rhitabratakarar <kararrhitabrata@gmail.com>
2024-02-08 23:12:42 +0530

    more formattings

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,16 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        'field-color': '#ffffff99'
-      }
+        "field-color": "#ffffff99",
+      },
     },
   },
   plugins: [],
-}
+};
