26746ada99404050162532b31670d6abe723ffd0
rhitabratakarar <kararrhitabrata@gmail.com>
2024-02-14 21:18:00 +0530

    changed margins between media and source url

--- components/RightSection/PlayableMedia.tsx
+++ components/RightSection/PlayableMedia.tsx
@@ -1,3 +1,3 @@
 export default function PlayableMedia() {
-  return <video className="bg-slate-200 rounded-lg w-[250px] h-[400px] my-6" />;
+  return <video className="bg-slate-200 rounded-lg w-[250px] h-[400px] my-2" />;
 }
--- components/RightSection/SourceURL.tsx
+++ components/RightSection/SourceURL.tsx
@@ -1,10 +1,20 @@
 export default function SourceURL() {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <p className="inline-block text-white">source url</p>
-      <div className="text-customSm border-none rounded-xl bg-field-color max-w-32 overflow-hidden min-h-10 max-h-14 break-words py-2 px-3 relative">
-        abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz
-        <div className="absolute h-2 w-2 bg-black right-0 "></div>
+    <div className="flex flex-col">
+      <p className="inline-block text-white m-auto mb-1">source url</p>
+
+      {/* <div className="text-customSm border-none rounded-xl bg-field-color w-full overflow-hidden max-h-14 break-words py-2 px-3">
+        <div className="w-full h-full relative">
+
+          abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz
+          <div className="h-2 w-2 bg-black z-10 right-2 rounded-full absolute"></div>
+          
+        </div>
+      
+      </div> */}
+
+      <div className="w-11/12 select-none bg-field-color rounded-full px-3 m-auto mb-3 mt-1">
+        <input value={"abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"} className="w-full bg-transparent outline-none cursor-default select-none text-customSm" readOnly={true}/>
       </div>
     </div>
   );
